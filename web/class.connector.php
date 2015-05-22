131d5bb4cd8562f05434b826b3a00051eefa8be9
Brendan Abolivier <brendan.abolivier@isen-bretagne.fr>
2015-05-22 16:42:46 +0200

    Ajout de la méthode Select du connecteur

--- web/class.connector.php
+++ web/class.connector.php
@@ -1,6 +1,6 @@
 <?php 
 
-class Connector() {
+class Connector {
 
 	private $bdd;
 	
@@ -20,16 +20,54 @@ class Connector() {
 			"where" => array(
 				array("foo", "=", "bar"),
 				array("blbl", ">", 5)
-			);
-			"order by" => array(0, 10, "desc");
+			),
+			"order by" => array("foo", "desc"),
+			"limit" => array(0, 10) // Ou array(10)
 		);
 	*/
-	function Select($fields, $table, $options) {
-		$request = "SELECT $fields FROM $table ";
+	function Select($fields, $tables, $options) {
+		$request = "SELECT $fields FROM $tables ";
 		$arrayVerif = array();
 		foreach($options as $name=>$value) {
-/*			$request += " $name :$name";
-			$arrayVerif[":$name"] = $value;*/
+			if(($upName = strtoupper($name)) == "WHERE") {
+				$whereClause = " $upName ";
+				foreach($value as $array) {
+					if(sizeof($array) != 3) {
+						throw new Exception('Nombre de paramètres incorrect (WHERE). Les paramètres passés sont : '
+							.implode(',',$array));
+					}
+
+					$whereClause .= $array[0]." ".$array[1]." :".$array[0]." AND ";
+					$arrayVerif[":".$array[0]] = $array[2];
+				}
+				if($substring = substr($whereClause, 0, -5)) {
+					$request .= $substring;
+				} else {
+					throw new Exception('Problème lors de la création du substring');
+				}
+			} else if(($upName = strtoupper($name)) == "ORDER BY") {
+				if(sizeof($value) != 2) {
+					throw new Exception('Nombre de paramètres incorrects (ORDER BY). Les paramètres passés sont : '
+						.implode(',', $value));
+				}
+
+				$request .= " ".$upName." ".implode(' ', $value);
+			} else if(($upName = strtoupper($name)) == "LIMIT") {
+				if(sizeof($value) == 1) {
+					// La colonne "limit" ne contient qu'un nombre de champs
+					$request .= " $upName ".$value[0];
+				} else if(sizeof($value) == 2) {
+					// La colonne "limit" contient un index de départ et un nombre de champs
+					$request .= " $upName ".$value[0].",".$value[1];
+				} else {
+					throw new Exception('Nombre de paramètres incorrects (LIMIT). Les paramètres passés sont : '
+						.implode(',', $value));
+				}
+			} else {
+				throw new Exception('Argument '.strtoupper($name).' inconnu');
+			}
 		}
+
+		return $bdd->prepare($request)->execute($arrayVerif)->fetchAll();
 	}
 }
