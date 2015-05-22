02a4443615809bf200a0e18091d97738ae258c54
Brendan Abolivier <brendan.abolivier@isen-bretagne.fr>
2015-05-22 19:42:20 +0200

    Test du connecteur dans l'index.php

--- web/index.php
+++ web/index.php
@@ -1,5 +1,20 @@
 <?php
-	header("Content-Type:application/json");
 
-	echo json_encode(array("Message" => "Hello world;"));
+require_once('classes.php');
 
+//	header("Content-Type:application/json");
+
+//	echo json_encode(array("Message" => "Hello world;"));
+
+
+$bdd = new Connector();
+
+$options = array(
+	"where" => array(
+		array("login", "=", "Dragavnir"),
+	),
+	"order by" => array("score", "desc"),
+	"limit" => array(0, 10) // Ou array(10)
+);
+
+var_dump($bdd->Select("*", "scores", $options));
