fb7a6bad95630403b8795de27ced4eab44488e20
PCYoshi <bidon>
2015-05-22 19:09:58 +0200

    Le sql qui marchegit add --allgit add --all!

--- sql/CTbq.sql
+++ sql/CTbq.sql
@@ -1,30 +1,35 @@
-DROP TABLE IF EXISTS question;
+DROP TABLE IF EXISTS questions;
 DROP TABLE IF EXISTS reponses;
 DROP TABLE IF EXISTS categorie;
 DROP TABLE IF EXISTS scores;
 
-CREATE TABLE categorie (
-	nom_cat		VARCHAR(30) PRIMARY KEY
-) ENGINE=INNODB;
+CREATE TABLE categorie(
+        nom_cat Varchar (30) NOT NULL ,
+        PRIMARY KEY (nom_cat )
+)ENGINE=InnoDB;
 
-CREATE TABLE reponses (
-	reponse1	VARCHAR(50),
-	reponse2	VARCHAR(50),
-	nom_cat		VARCHAR(30) NOT NULL,
-	PRIMARY KEY(reponse1, reponse2),
-	FOREIGN KEY(nom_cat) REFERENCES categorie(nom_cat) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=INNODB;
+CREATE TABLE reponses(
+        reponse1 Varchar (50) NOT NULL ,
+        reponse2 Varchar (50) NOT NULL ,
+        nom_cat  Varchar (30) ,
+        INDEX (reponse2),
+        PRIMARY KEY (reponse1 ,reponse2 )
+)ENGINE=InnoDB;
 
-CREATE TABLE question (
-	intitule	VARCHAR(120),
-	reponse1	VARCHAR(50),
-	reponse2	VARCHAR(50),
-	reponse		INTEGER NOT NULL,
-	PRIMARY KEY(intitule, reponse1, reponse2),
-	FOREIGN KEY(reponse1, reponse2) REFERENCES reponses(reponse1, reponse2) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=INNODB;
+CREATE TABLE questions(
+        intitule    Varchar (150) NOT NULL ,
+        num_reponse TinyINT ,
+        reponse1    Varchar (50) ,
+        reponse2    Varchar (50) ,
+        PRIMARY KEY (intitule,reponse1,reponse2 )
+)ENGINE=InnoDB;
 
-CREATE TABLE scores (
-	login		VARCHAR(50) PRIMARY KEY,
-	score		INTEGER NOT NULL
-) ENGINE=INNODB;
+CREATE TABLE scores(
+        login Varchar (20) NOT NULL ,
+        score Int ,
+        PRIMARY KEY (login )
+)ENGINE=InnoDB;
+
+ALTER TABLE reponses ADD CONSTRAINT FK_Reponses_nom_cat FOREIGN KEY (nom_cat) REFERENCES categorie(nom_cat) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE questions ADD CONSTRAINT FK_Questions_reponse2 FOREIGN KEY (reponse2) REFERENCES reponses(reponse2) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE questions ADD CONSTRAINT FK_Questions_reponse1 FOREIGN KEY (reponse1) REFERENCES reponses(reponse1) ON DELETE CASCADE ON UPDATE CASCADE;
