160d60ef0005242b31b16e87bc1f8d20190252ce
Brendan Abolivier <contact@brendanabolivier.com>
2015-05-22 18:23:06 +0000

    Bye index testing, hello palmares application of the Connector class

--- web/index.php
+++ web/index.php
@@ -2,19 +2,7 @@
 
 require_once('classes.php');
 
-//	header("Content-Type:application/json");
+header("Content-Type:application/json");
 
-//	echo json_encode(array("Message" => "Hello world;"));
+echo json_encode(array("Message" => "Hello world;"));
 
-
-$bdd = new Connector();
-
-$options = array(
-	"where" => array(
-		array("login", "=", "Dragavnir"),
-	),
-	"order by" => array("score", "desc"),
-	"limit" => array(0, 10) // Ou array(10)
-);
-
-var_dump($bdd->Select("*", "scores", $options));
