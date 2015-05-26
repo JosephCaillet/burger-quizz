21fb163c7edc18603053f6fc113dcb8925ebf54e
PCYoshi <bidon>
2015-05-26 10:26:51 +0200

    evolution

--- java/src/InterfacePrincipale.java
+++ java/src/InterfacePrincipale.java
@@ -1,6 +1,39 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class InterfacePrincipale extends JFrame
 {
+	//Panel des catégories
+	private JPanel panCategories;
+	private Bouton addC;
+	private Bouton delC;
+	private Bouton editC;
+	private JList listC;
 
+	//Panel des réponses
+	private JPanel panReponses;
+	private Bouton addR;
+	private Bouton delR;
+	private Bouton editR;
+	private JComboBox comboRepCat;
+	private JList listR;
+
+	//Panel des questions
+	private JPanel panQuestions;
+	private Bouton addQ;
+	private Bouton delQ;
+	private Bouton editQ;
+	private JComboBox comboQueRep;
+	private JList list;
+
+	public InterfacePrincipale()
+	{
+		setLocationRelativeTo(null);
+		setTitle("Administration base de données de l'aplication BurgerQuizz ce titre est trop long");
+		setVisible(true);
+	}
+
+	private void createPanelCategories(){}
+	private void createPanelReponses(){}
+	private void createPanelQuestion(){}
 }
---
+++ java/src/MainAdminBddBurgerQuizz.java
@@ -0,0 +1,7 @@
+public class MainAdminBddBurgerQuizz
+{
+	public static void main(String args[])
+	{
+		InterfacePrincipale alain_chabat = new InterfacePrincipale();
+	}
+}
