9c10016f452e01f1f1a049de5e98c1aa9541f604
Brendan Abolivier <contact@brendanabolivier.com>
2015-05-26 13:02:56 +0200

    Fixed insertions

--- sql/CTITbq.sql
+++ sql/CTITbq.sql
@@ -57,24 +57,24 @@ CREATE TABLE IF NOT EXISTS `questions` (
 --
 
 INSERT INTO `questions` (`intitule`, `num_reponse`, `reponse1`, `reponse2`) VALUES
-('2 d''entre eux ont tapé ce texte:', 2, 'Les CSI', 'Les CIR'),
-('A été diffusée pendant les années 80:', 0, 'Les Mystérieuses Cités d''Or', 'Les Tortues Ninjas'),
-('C''est un héros de Nintendo:', 0, 'Mario', 'Link'),
-('Elle est accompagnée d’animaux:', 1, 'Gigi', 'Magical DoReMi'),
-('Elle possède une fée nommée Dodo:', 2, 'Gigi', 'Magical DoReMi'),
-('Il à révé sur l'' île de Cocolint:', 2, 'Mario', 'Link'),
-('Il protège le royaume champignon:', 1, 'Mario', 'Link'),
-('Ils passent en colle:', 1, 'Les CSI', 'Les CIR'),
-('Les clubs y sont situés:', 1, 'Rez de chaussée', '1er étage'),
-('L’héroïne possède des pouvoirs magique:', 0, 'Gigi', 'Magical DoReMi'),
-('On y affronte une tortue à trois tête:', 1, 'A Link to the Past', 'Ocarina of Time'),
-('On y aperçoit un bateau nommé Solaris:', 1, 'Les Mystérieuses Cités d''Or', 'Les Tortues Ninjas'),
-('On y combat des Skulltulas:', 2, 'A Link to the Past', 'Ocarina of Time'),
-('On y ramasse des fragment de cœur:', 0, 'A Link to the Past', 'Ocarina of Time'),
-('On y trouve des salles de cours:', 0, 'Rez de chaussée', '1er étage'),
-('Ont des cours de Maths/Physique:', 0, 'Les CSI', 'Les CIR'),
-('Un certain Shreder y intervient:', 2, 'Les Mystérieuses Cités d''Or', 'Les Tortues Ninjas'),
-('Une salle consacrée aux devoir s''y situe:', 2, 'Rez de chaussée', '1er étage');
+('2 d''entre eux ont tapé ce texte', 2, 'Les CSI', 'Les CIR'),
+('A été diffusée pendant les années 80', 0, 'Les Mystérieuses Cités d''Or', 'Les Tortues Ninjas'),
+('C''est un héros de Nintendo', 0, 'Mario', 'Link'),
+('Elle est accompagnée d’animaux', 1, 'Gigi', 'Magical DoReMi'),
+('Elle possède une fée nommée Dodo', 2, 'Gigi', 'Magical DoReMi'),
+('Il à révé sur l'' île de Cocolint', 2, 'Mario', 'Link'),
+('Il protège le royaume champignon', 1, 'Mario', 'Link'),
+('Ils passent en colle', 1, 'Les CSI', 'Les CIR'),
+('Les clubs y sont situés', 1, 'Rez de chaussée', '1er étage'),
+('L’héroïne possède des pouvoirs magique', 0, 'Gigi', 'Magical DoReMi'),
+('On y affronte une tortue à trois tête', 1, 'A Link to the Past', 'Ocarina of Time'),
+('On y aperçoit un bateau nommé Solaris', 1, 'Les Mystérieuses Cités d''Or', 'Les Tortues Ninjas'),
+('On y combat des Skulltulas', 2, 'A Link to the Past', 'Ocarina of Time'),
+('On y ramasse des fragment de cœur', 0, 'A Link to the Past', 'Ocarina of Time'),
+('On y trouve des salles de cours', 0, 'Rez de chaussée', '1er étage'),
+('Ont des cours de Maths/Physique', 0, 'Les CSI', 'Les CIR'),
+('Un certain Shreder y intervient', 2, 'Les Mystérieuses Cités d''Or', 'Les Tortues Ninjas'),
+('Une salle consacrée aux devoir s''y situe', 2, 'Rez de chaussée', '1er étage');
 
 -- --------------------------------------------------------
 
@@ -116,11 +116,16 @@ CREATE TABLE IF NOT EXISTS `scores` (
 --
 
 INSERT INTO `scores` (`login`, `score`) VALUES
-('Jean', 85),
-('kikou37', 14),
-('Micheline', 85),
+('azerty', 28),
+('David', 19),
+('Dragavnir', 666),
+('Finistère', 29),
+('Joseph', 42),
+('K.S.', 20),
+('Loic', 10),
+('Marc F.', 0), 
 ('Patrick', 24),
-('thefifou', 56);
+('Thomas', 33);
 
 --
 -- Index pour les tables exportées
