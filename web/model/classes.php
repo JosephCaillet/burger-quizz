e740ab498e24166beb31fd2652bbe7e280c74607
Brendan Abolivier <contact@brendanabolivier.com>
2015-05-26 11:33:33 +0200

    Rectification du classes.php

--- web/model/classes.php
+++ web/model/classes.php
@@ -1,5 +1,6 @@
 <?php
 
-require_once('class.connector.php');
-require_once('class.questions.php');
-require_once('class.questset.php');
+// Call this file from the root folder
+require_once('model/class.connector.php');
+require_once('model/class.questions.php');
+require_once('model/class.questset.php');
