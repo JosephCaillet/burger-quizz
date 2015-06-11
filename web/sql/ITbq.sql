2fb36ece9613084a30fb1fbba2d80a534b511ccd
Brendan Abolivier <contact@brendanabolivier.com>
2015-06-11 21:23:23 +0200

    On remet un fichier SQL au niveau des autres.

--- web/sql/ITbq.sql
+++ web/sql/ITbq.sql
@@ -1,37 +1,8 @@
--- phpMyAdmin SQL Dump
--- version 4.2.12deb2
--- http://www.phpmyadmin.net
---
--- Client :  localhost
--- Généré le :  Sam 23 Mai 2015 à 15:15
--- Version du serveur :  5.5.43-0+deb8u1
--- Version de PHP :  5.6.7-1
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
---
--- Base de données :  `burgerquizz`
---
-
---
--- Contenu de la table `categorie`
---
-
 INSERT INTO `categorie` (`nom_cat`) VALUES
 ('Dessin Animés'),
 ('ISEN'),
 ('Jeux Vidéos');
 
---
--- Contenu de la table `questions`
---
 
 INSERT INTO `questions` (`intitule`, `num_reponse`, `reponse1`, `reponse2`) VALUES
 ('2 d''entre eux ont tapé ce texte', 2, 'Les CSI', 'Les CIR'),
@@ -39,11 +10,11 @@ INSERT INTO `questions` (`intitule`, `num_reponse`, `reponse1`, `reponse2`) VALU
 ('C''est un héros de Nintendo', 0, 'Mario', 'Link'),
 ('Elle est accompagnée d’animaux', 1, 'Gigi', 'Magical DoReMi'),
 ('Elle possède une fée nommée Dodo', 2, 'Gigi', 'Magical DoReMi'),
-('Il à révé sur l'' île de Cocolint', 2, 'Mario', 'Link'),
+('Il a rêvé sur l''île de Cocolint', 2, 'Mario', 'Link'),
 ('Il protège le royaume champignon', 1, 'Mario', 'Link'),
 ('Ils passent en colle', 1, 'Les CSI', 'Les CIR'),
 ('Les clubs y sont situés', 1, 'Rez de chaussée', '1er étage'),
-('L’héroïne possède des pouvoirs magique', 0, 'Gigi', 'Magical DoReMi'),
+('L''héroïne possède des pouvoirs magique', 0, 'Gigi', 'Magical DoReMi'),
 ('On y affronte une tortue à trois tête', 1, 'A Link to the Past', 'Ocarina of Time'),
 ('On y aperçoit un bateau nommé Solaris', 1, 'Les Mystérieuses Cités d''Or', 'Les Tortues Ninjas'),
 ('On y combat des Skulltulas', 2, 'A Link to the Past', 'Ocarina of Time'),
@@ -53,9 +24,6 @@ INSERT INTO `questions` (`intitule`, `num_reponse`, `reponse1`, `reponse2`) VALU
 ('Un certain Shreder y intervient', 2, 'Les Mystérieuses Cités d''Or', 'Les Tortues Ninjas'),
 ('Une salle consacrée aux devoir s''y situe', 2, 'Rez de chaussée', '1er étage');
 
---
--- Contenu de la table `reponses`
---
 
 INSERT INTO `reponses` (`reponse1`, `reponse2`, `nom_cat`) VALUES
 ('Gigi', 'Magical DoReMi', 'Dessin Animés'),
@@ -65,9 +33,6 @@ INSERT INTO `reponses` (`reponse1`, `reponse2`, `nom_cat`) VALUES
 ('A Link to the Past', 'Ocarina of Time', 'Jeux Vidéos'),
 ('Mario', 'Link', 'Jeux Vidéos');
 
---
--- Contenu de la table `scores`
---
 
 INSERT INTO `scores` (`login`, `score`) VALUES
 ('azerty', 28),
@@ -80,7 +45,3 @@ INSERT INTO `scores` (`login`, `score`) VALUES
 ('Marc F.', 0),
 ('Patrick', 24),
 ('Thomas', 33);
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
